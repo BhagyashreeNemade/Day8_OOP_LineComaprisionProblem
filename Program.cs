6336b93e0a0416b12077c4b79f8957b287a7919f
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-08-05 15:57:22 +0530

    Comparison Using All Methods

--- Program.cs
+++ Program.cs
@@ -5,12 +5,41 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to the line comparison");
-            //calculatinglinelength a = new calculatinglinelength();
-            //a.Calculatinglinelength();
-            //LinesisEqualorNot b = new LinesisEqualorNot();
-            //b.linesequalornot();
-            TwoLinesComparison c = new TwoLinesComparison();
-            c.Comparison();
-        }   
+            Console.WriteLine("Enter the choice");
+            Console.WriteLine(" 1.calculatinglinelength");
+            Console.WriteLine(" 2.LinesisEqualorNot");
+            Console.WriteLine(" 3.TwoLinesComparison");
+            Console.WriteLine(" 4.ComparisonUsingAllMethods");
+
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    calculatinglinelength a = new calculatinglinelength();
+                    a.Calculatinglinelength();
+
+                    break;
+                case 2:
+                    LinesisEqualorNot b = new LinesisEqualorNot();
+                    b.linesequalornot();
+                    break;
+                case 3:
+                    TwoLinesComparison c = new TwoLinesComparison();
+                    c.Comparison();
+                    break;
+
+                case 4:
+                    ComparisonUsingAllMethods d = new ComparisonUsingAllMethods();
+                    d.AllComparison();
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter the correct choice");
+                    break;
+            }
+
+        }
     }
 }
