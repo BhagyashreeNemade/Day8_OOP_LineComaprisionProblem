1b707caee79ffb8970cdbf687842679d85d8b90a
bhagyashree2000 <bhagyashreenemade2000@gmail.com>
2022-08-05 14:35:19 +0530

    greeting message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparison
+{
+    class Program
+    {
+        public static double length1, length2;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to the line comparison");
+        }
+    }
+}
